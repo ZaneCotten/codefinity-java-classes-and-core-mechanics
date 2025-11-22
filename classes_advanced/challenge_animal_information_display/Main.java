6ca3c0e0ec297d5417576fb5f844fc7c62ceb384
ZaneCotten <>
2025-11-22 18:11:59 +0000

    Challenge: Animal Information Display

---
+++ classes_advanced/challenge_animal_information_display/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal cat = new Animal("Cat", "Garfield", "Red");
+        Animal dog = new Animal("Dog", "Maks", "Black");
+
+        System.out.println(cat + "\n" + dog);
+    }
+}
+
+class Animal {
+    private String type;
+    private String name;
+    private String color;
+
+    public Animal(String type, String name, String color){
+        this.type = type;
+        this.name = name;
+        this.color = color;
+    }
+    
+    @Override
+    public String toString() {
+        return "Animal{" + "type='" + type + '\'' + ", name='" + name + '\'' + ", color='" + color + '\'' + '}';
+    }
+}
